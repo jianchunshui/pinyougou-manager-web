9fc519a7a5ee30d8b0404332a8fbe2b289ed3b13
jcsnb <18179008275@163.com>
2019-05-26 16:58:43 +0800

    第一次提交

---
+++ src/main/webapp/js/controller/brandController.js
@@ -0,0 +1,69 @@
+app.controller("brandController",function($scope,$http,$controller,brandService){
+			$controller('baseController',{$scope:$scope})
+    			$scope.findAll=function(){
+    				brandService.findAll().success(
+    					function(response){
+    						$scope.list=response;
+    					}		
+    				);
+    			}
+    			
+    			$scope.findPage=function(page,size){
+    				brandService.findPage(page,size).success(
+    					function(response){
+    						$scope.list=response.rows;
+    						$scope.paginationConf.totalItems=response.total;
+    					}		
+    				);
+    			}
+    			
+    			//新增
+    			$scope.save=function(){
+    				var object = null;
+    				if($scope.entity.id!=null){
+    					object =brandService.update($scope.entity); 	 
+    				}else{
+    					object =brandService.add($scope.entity); 	 
+    				}
+    				object.success(
+    					function(response){
+    						if(response.success){
+    							$scope.reloadList();//刷新
+    						}else{
+    							alert(response.message);
+    						}
+    					}		
+    				)
+    			}
+    			
+    			$scope.findOne=function(id){
+    				brandService.findOne(id).success(
+    					function(response){
+    						$scope.entity=response;
+    					}		
+    				)
+    			}
+    			
+    			$scope.dele=function(){
+    				brandService($scope.selectIds).success(
+    					function(response){
+    						if(response.success){
+    							$scope.reloadList();//刷新
+    						}else{
+    							alert(response.message);
+    						}
+    					});	
+    				}
+    			
+    			$scope.searchEntity={};
+    			$scope.search=function(page,size){
+    				brandService.search(page,size,$scope.searchEntity).success(
+        					function(response){
+        						$scope.list=response.rows;
+        						$scope.paginationConf.totalItems=response.total;
+        					}		
+        				);
+    				}
+    			
+    			
+    		}); 
